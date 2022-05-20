e89405654530adee4c9473105e6b90719a2c7ef4
unknown <mindsdet321@iiht.tech>
2022-05-20 15:45:28 +0530

    Comprehensive1

---
+++ Desktop/git files/git files/src/main/java/Mindtree/MavenAssessment/App.java
@@ -0,0 +1,13 @@
+package Mindtree.MavenAssessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
